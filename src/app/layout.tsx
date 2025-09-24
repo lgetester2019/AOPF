f17f7772d748ed91dd1a3213e7b070333b7021c8
lgetester2019 <lgetester2019@gmail.com>
2025-09-24 11:02:29 +0300

    Update layout.tsx
    Add YAMtrika via env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+const YANDEX_METRIKA_ID = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID
 
 export const metadata: Metadata = {
     title: "Охрана труда и безопасность — лаборатория с аккредитацией",
@@ -38,9 +39,39 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ru">
-        <body className="antialiased">
-        {children}
-        </body>
+            <head>
+                {YANDEX_METRIKA_ID && (
+                  <script
+                    dangerouslySetInnerHTML={{
+                      __html: `
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                        
+                        ym(${YANDEX_METRIKA_ID}, "init", {
+                          clickmap:true,
+                          trackLinks:true,
+                          accurateTrackBounce:true,
+                          webvisor:true
+                        });
+                      `,
+                    }}
+                  />
+                )}
+            </head>
+            <body className="antialiased">
+                {children}
+                {YANDEX_METRIKA_ID && (
+                  <noscript>
+                    <div>
+                      <img src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} 
+                       style={{position:'absolute', left:'-9999px'}} alt="" />
+                    </div>
+                  </noscript>
+                )}
+            </body>
         </html>
     );
 }
